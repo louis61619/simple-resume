d1f53c34e5960d4551dce638c19a56b1832ceb4c
louis61619 <louis61619@gmail.com>
2022-11-18 17:50:58 +0800

    11/18

--- picker.js
+++ picker.js
@@ -3,9 +3,6 @@
 /* exported handleAuthClick */
 /* exported handleSignoutClick */
 
-// 如何持久化登入狀態
-// 寫入數據
-
 
 // Authorization scopes required by the API; multiple scopes can be
 // included, separated by spaces.
@@ -30,6 +27,8 @@ let fileId =localStorage.getItem('fileId') || null;
 if (accessToken) {
   // document.getElementById("signout_button").style.visibility = "visible";
   document.getElementById("authorize_dialog").style.display = "none";
+  document.getElementById("new_dialog").style.display = "none";
+  document.getElementById("dialog_bg").style.display = "none";
   if (fileId) {
     document.getElementById("folder_dialog").style.display = "none";
   }
@@ -37,6 +36,7 @@ if (accessToken) {
   // 初始化
   // document.getElementById("loading").style.display = "none";
   document.getElementById("folder_dialog").style.display = "none";
+  document.getElementById("new_dialog").style.display = "none";
   // document.getElementById("signout_button").style.visibility = "hidden";
 }
 
@@ -77,18 +77,34 @@ function gisLoaded() {
     accessToken = response.access_token;
     localStorage.setItem('accessToken', accessToken)
     if (fileId) {
-      const res = await gapi.client.drive.files.get({
+      gapi.client.drive.files.get({
         fileId: fileId,
         // fields: "*",
         alt: 'media'
+      }).then(res => {
+        text.value = res.body
+        content.innerHTML = marked.parse(res.body);
       });
-      text.value = res.body
-      content.innerHTML = marked.parse(res.body);
+      getFileDescription(fileId).then(res => {
+        document.getElementById('resume_name').value = res.name.replace(/\.md$/, '')
+      })
+
     }
     
   };
   gisInited = true;
   maybeEnableButtons();
+}
+
+async function getFileDescription (driveId) {
+  return new Promise( (resolve, reject) => {
+    gapi.client.drive.files.get({
+      'fileId': driveId,
+      'fields': 'id,version,name,appProperties',
+    }).execute(
+      (response) => resolve(response)
+    );
+  });
 }
 
 /**
@@ -106,39 +122,73 @@ function maybeEnableButtons() {
 }
 
 async function newResume() {
+  const filename = document.getElementById("new_resume_name").value
+  document.getElementById('resume_name').value = filename
   const fileMetadata = {
-    name: `${new Date().valueOf()}-simple-resume.md`,
+    name: filename + '.md',
   };
-  // const media = {
-  //   mimeType: 'application/json',
-  //   body: fs.createReadStream('files/config.json'),
-  // };
   try {
+    let str = '';
+    console.log(document.getElementById('with_template').checked)
+    if (document.getElementById('with_template').checked) {
+      str = sample
+    }
+    text.value = str
+    content.innerHTML = marked.parse(str);
+    
+    document.getElementById("new_dialog").style.display = "none";
+    document.getElementById("dialog_bg").style.display = "none";
+
     const file = await gapi.client.drive.files.create({
       resource: fileMetadata,
       // media: media,
       fields: 'id',
     });
+
     fileId = file.result.id
-    text.value = sample
-    content.innerHTML = marked.parse(sample);
     localStorage.setItem('fileId', file.result.id);
-    document.getElementById("folder_dialog").style.display = "none";
+
+    const res = await gapi.client.request({
+      'path': driveUploadPath + '/' + fileId,
+      'method': 'PATCH',
+      'params': {'uploadType': 'media', 'fields': 'id'},
+      'body': str
+    })
+    
   } catch (err) {
     // TODO(developer) - Handle error
+    if (err.status === 401) {
+      alert('登入時間過期')
+    }
     throw err;
   }
 }
 
+async function renameFile(driveId, newName) {
+  return new Promise( (resolve, reject) => {
+    gapi.client.drive.files.update({
+      'fileId': driveId,
+      'name': newName,
+      'fields': 'id'
+    }).then(
+      (response) => resolve(response.result),
+      reject
+    );
+  });
+}
+
 async function saveResume() {
-  const file = await gapi.client.request({
+  const promise1 = gapi.client.request({
     'path': driveUploadPath + '/' + fileId,
     'method': 'PATCH',
     'params': {'uploadType': 'media', 'fields': 'id'},
     'body': text.value
   })
-  alert('保存成功')
-  console.log(file)
+  const promise2 = renameFile(fileId, document.getElementById('resume_name').value)
+  Promise.all([promise1, promise2]).then((res) => {
+    alert('保存成功')
+    console.log(res)
+  })
 }
 
 /**
@@ -211,6 +261,11 @@ function createPicker() {
   picker.setVisible(true);
 }
 
+function createNewDialog() {
+  window.document.getElementById("new_dialog").style.display = "block";
+  window.document.getElementById("new_resume_name").value = `Untitled Resume`;
+}
+
 /**
  * Displays the file details of the user's selection.
  * @param {object} data - Containers the user selection from the picker
@@ -218,21 +273,25 @@ function createPicker() {
 async function pickerCallback(data) {
   if (data.action === google.picker.Action.PICKED) {
     window.document.getElementById("folder_dialog").style.display = "none";
+    window.document.getElementById("dialog_bg").style.display = "none";
     // let text = `Picker response: \n${JSON.stringify(data, null, 2)}\n`;
     const document = data[google.picker.Response.DOCUMENTS][0];
     fileId = document[google.picker.Document.ID];
     localStorage.setItem('fileId', fileId)
-    const res = await gapi.client.drive.files.get({
+
+    gapi.client.drive.files.get({
       fileId: fileId,
-      // fields: "*",
       alt: 'media'
+    }).then(res => {
+      text.value = res.body
+      content.innerHTML = marked.parse(res.body);
     });
-    // text += `Drive API response for first document: \n${JSON.stringify(
-    //   res.result,
-    //   null,
-    //   2
-    // )}\n`;
-    text.value = res.body
-    content.innerHTML = marked.parse(res.body);
+    getFileDescription(fileId).then(res => {
+      window.document.getElementById('resume_name').value = res.name.replace(/\.md$/, '')
+    })
   }
+}
+
+function closeDialog(el) {
+  el.parentElement.style.display = 'none'
 }
